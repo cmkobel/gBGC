f6e8b61123175510cbe1ee8fcf6b441de0fcc768
Carl M. Kobel <kobel@protonmail.ch>
2020-02-21 14:12:33 +0100

    binning should work now

--- workflow/script/xmfa_bin.py
+++ workflow/script/xmfa_bin.py
@@ -14,13 +14,9 @@ import glob
 import json
 import re
 
-#target_bin_size = 10   # 1K, 10K, 100K
 
 input_file = sys.argv[1]
-target_bin_size = int(sys.argv[2])
-#input_file_stem = '.'.join(input_file.split(".")[0:-1])
-#input_file_extension = input_file.split(".")[-1]
-#output_file = f"{input_file_stem}_binsize_{target_bin_size}.{input_file_extension}
+target_bin_size = int(sys.argv[2]) # 1K, 10K, 100K
 
 def eprint(*args, **kwargs):
     # I'm too lazy to write 'file = sys.stderr' manually...
@@ -33,7 +29,9 @@ XMFA_HEADER_REGEX = re.compile(r"> (?P<id>\d+):(?P<start>\d+)-(?P<end>\d+) (?P<s
 
 current_bin = {} # could also be named current_bin
 current_bin_size = 0
-n_bins = 1
+n_bins = 0
+number = 0
+last_end = 1
 
 with open(input_file, 'r') as open_input_file:
     for line in open_input_file:
@@ -42,38 +40,47 @@ with open(input_file, 'r') as open_input_file:
 
             # The last current header is irrelevant to the current bin as a whole, and may be deleted now.
             del current_header
+            number = 0
 
-            
-            # Check the bin size each time an alignment is gone by.
-            current_bin_size = [current_bin[key]['length'] for key in current_bin.keys()][0]
-            #eprint(f"current_bin_size (target_bin_size): {current_bin_size} ({target_bin_size})")
+            # Check that all sequences in the current bin have the same length.
+            current_bin_sizes = list(set([len(current_bin[key]) for key in current_bin.keys()]))
+            if len(current_bin_sizes) > 1:
+                raise Exception(f"Error: alignments of {current_id} do not have equal lengths. Observed lengths: {current_bin_sizes}.")
+            current_bin_size = current_bin_sizes[0]
+            eprint(f"id {current_id}: size {current_bin_size}")
+
 
             # Når current bin-size er nået (eller overskredet) skal indholdet skrives gemmes som et enkelt alignment, og current_bin kan slettes, så der kan startes forfra med en ny bin.
             if current_bin_size >= target_bin_size:
-                eprint('writing bin', n_bins, 'of size', current_bin_size)
+                n_bins += 1
+                eprint(f"writing bin {n_bins} of size {current_bin_size}\n")
                 #eprint(json.dumps(current_bin, indent = 4))
                 # Skriv bin
-                for key in current_bin.keys():
-                    print(f"> {key}\n{current_bin[key]['sequence']}")
+                for key in sorted(current_bin.keys()):
+                    print(f"> {current_id}:{last_end}-{last_end+current_bin_size-1} + bin {n_bins}\n{current_bin[key]}")
                 print('=')
-
-                # Reset the current bin                
-                current_bin = {}
-                n_bins += 1
                 
-                exit()
+                last_end += current_bin_size
+
+                # Reset 
+                current_bin = {}
+                del current_id
+                
+                del current_bin_sizes
+                del current_bin_size
 
                     
         elif line[0] == ">": # A new "fasta" header commences.
             m = re.match(XMFA_HEADER_REGEX, line)
+            number += 1
             #eprint(m)
 
 
             # Let's check that the id actually exists.
-            parsed_id = m.group('id')
-            eprint(parsed_id)
+            current_id = m.group('id')
+            #eprint('id', current_id)
 
-            
+            # Read metadata
             current_header = {} 
             for key in ("start", "end", "id", "strand", "comments", "realname"): 
                 try: 
@@ -94,22 +101,39 @@ with open(input_file, 'r') as open_input_file:
 
             #eprint(json.dumps(current_header, indent = 4))
         
+
         else: # DNA sequence data for which we should (hopefully) already have obtained the metadata.
-            if not current_header['id'] in current_bin:
-                current_bin[current_header['id']] = {'sequence': '', 'length': 0}
 
+            if not number in current_bin:
+                current_bin[number] = "" 
 
-
-            current_bin[current_header['id']]['sequence'] += line.strip()
-            current_bin[current_header['id']]['length'] += len(line.strip())
+            current_bin[number] += line.strip()
             
-
+            
 
 # The last sequence might not necessarily have been written to file.
 # Let's do that.
-if len(current_bin.keys()) >= 0:
-    eprint('writing bin', n_bins, 'of size', current_bin_size)
-    for key in current_bin.keys():
-        print(f"> {key}\n{current_bin[key]['sequence']}")
-        print('=')
+if len(current_bin.keys()) > 0:
+    n_bins += 1
+    eprint('writing last bin', n_bins, 'of size', current_bin_size)
+    #eprint(json.dumps(current_bin, indent = 4))
+    # Skriv bin
+    for key in sorted(current_bin.keys()):
+        #print(f"> {current_id}\n{current_bin[key]}")
+        #print(f"> {current_id}:{last_end}-{last_end+current_bin_size-1}\n{current_bin[key]}")
+        print(f"> {current_id}:{last_end}-{last_end+current_bin_size-1} + bin {n_bins}\n{current_bin[key]}")
+    print('=')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
