c2bfabfafcadd8ab9c9905d7fef3d531dbe7ad07
Carl M. Kobel <kobel@protonmail.ch>
2020-03-11 15:58:24 +0100

    amend

--- workflow/compare.r
+++ workflow/compare.r
@@ -6,7 +6,7 @@ library(tidyverse)
 æs = aes
 setwd("~/urecomb/lokal/exports/export4compare/")
 
-'
+
 ## GC
 gather_gc = function(whatever = NULL) {
     gc_files = list.files(path=".", pattern="*bp_gc.tab", full.names=TRUE, recursive=T)
@@ -24,7 +24,7 @@ gather_gc = function(whatever = NULL) {
         summarise(GC3 = mean(gc_content))
 }
 gc_data_summarised = gather_gc()
-'
+
 
 ## mcorr    import fitted (recombination) parameters
 gather_mcorr = function() {
@@ -51,6 +51,7 @@ mcorr_data = gather_mcorr()
 
 ## PHI files
 gather_PHI = function() {
+    alpha = 0.05
     phi_files = list.files(path=".", pattern="*phi_results.tab", full.names=TRUE, recursive=T)
     phi_data = tibble()
     i = 1
@@ -61,16 +62,19 @@ gather_PHI = function() {
         rm(import)
         i = i + 1
     }
+        "spread(method, pvalue) %>% 
+        rename(p_maxchisq = 'Max Chi^2:',
+               p_nss = 'NSS NA:',
+               p_phi_normal = 'PHI (Normal):',
+               p_phi_permut = 'PHI (Permutation):')
+        "
     phi_data %>%
         mutate(method = paste(method, detail),
                unitig = str_sub(genome, 8, 8),
                genospecies = str_sub(genome, 24, 24)) %>% 
         select(-detail) %>% 
-        spread(method, pvalue) %>% 
-        rename(p_maxchisq = 'Max Chi^2:',
-               p_nss = 'NSS NA:',
-               p_phi_normal = 'PHI (Normal):',
-               p_phi_permut = 'PHI (Permutation):')
+        group_by(method, unitig, genospecies) %>% 
+        mutate(recombine = if_else(pvalue < alpha/length(pvalue), T, F), n_genes = length(pvalue))
 }
 phi_data = gather_PHI() 
 
@@ -79,7 +83,27 @@ phi_data = gather_PHI()
 # It doen't make sense to bin, because we don't care about GC.
 
 
-data = inner_join(mcorr_data, phi_data) %>% select(-genome)
+data = inner_join(inner_join(gc_data_summarised, phi_data), mcorr_data) %>% select(-genome)
+
+
+## Let's check that phi measures OK when concatenating the genes. (Remember, we have to concatenate the genes in order to be able to measure anything).
+
+# We have to drop na's, or many bins will fall out.
+data = data %>% drop_na()
+
+# bin the GC content
+
+data_binned20 = data %>% 
+    group_by(genospecies, unitig) %>% 
+    mutate(GC3_bin = cut_number(GC3, 20)) %>% 
+    group_by(GC3_bin, add = T) %>%
+    mutate(mean_GC3 = mean(GC3)) %>% # View
+    group_by(genospecies, unitig, mean_GC3) %>%  # use mean instead of weird bin-range
+    count(recombine) %>%
+    spread(recombine, n, fill = 0) %>% 
+    rename(n_recombining = `TRUE`, n_not_recombining = `FALSE`) %>% 
+    mutate(ratio = n_recombining / (n_recombining + n_not_recombining))
+
 
 # main plot
 data %>% ggplot(æs(phi_pool, p_phi_permut)) + 
@@ -91,10 +115,10 @@ data %>% ggplot(æs(phi_pool, p_phi_permut)) +
 
 
 ## Let's filter the mcorr tails away
-tail_size_phi_pool = 0.1
-tail_size_ratio = 0.01
-tail_size_c = 0.00
-tail_size_phi_s = 0.02
+tail_size_phi_pool = 0.0 #0.1
+tail_size_ratio = 0.0 #0.01
+tail_size_c = 0.00 # 0.00
+tail_size_phi_s = 0.0 #0.02
 
 data_inliers = data %>%
     group_by(bin_size, genospecies, unitig) %>% 
@@ -109,18 +133,29 @@ data_inliers = data %>%
 # sanity check of tail-removal
 # various histograms
 data_inliers %>%
-    filter(bin_size == 20000) %>% 
-    #pivot_longer(c(phi_pool, mcorr_ratio, mcorr_c, phi_s)) %>% 
-    ggplot(aes(phi_pool, fill = genospecies)) +
+    #filter(bin_size == 20000) %>% 
+    pivot_longer(c(phi_pool, mcorr_ratio, mcorr_c, phi_s)) %>% 
+    ggplot(aes(value, fill = genospecies)) +
     geom_histogram() + 
-    facet_grid(name~genospecies)
+    facet_grid(name~genospecies) + 
+    labs(caption = "Only unitig 0 and bin size 20K is included in this dataset.")
 
 
 # main plot
-data_inliers %>%
+# mcorr_phi_pool and PHI
+data_inliers %>% 
     #filter(bin_size == 30000) %>% 
-    ggplot(aes(GC3, log10(phi_pool), color = gs)) +
+    ggplot(aes(p_phi_permut, (mcorr_ratio), color = genospecies)) +
     geom_point(alpha = 0.5) + 
-    geom_smooth() +
-    facet_grid(bin_size~gs)
+    geom_smooth(formula = y~x, method = "lm") +
+    facet_grid(.~genospecies, scales = "free") +
+    theme(axis.text.x = element_text(vjust = 0.5, angle = -90)) +
+    labs(title = "")
+ggsave("main_compare.pdf")
+
+
+
+# Try binning by GC content then compare PHI ratio and median phi_pool
+
+
 
