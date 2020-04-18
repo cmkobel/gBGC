767deda236075d5bccb0ff01f86eea708e838d8c
Carl M. Kobel <kobel@protonmail.ch>
2020-04-18 20:20:37 +0200

    ran with this

--- workflow_fresh/workflow.py
+++ workflow_fresh/workflow.py
@@ -24,13 +24,44 @@ gwf = gwf.Workflow(defaults={
 base_path = f"/home/cmkobel/gBGC/carl/workflow_fresh"
 tree_base = f"data/tabseq/genospecies_core_genomes"
 
+
+GC3_output_file = f"{base_path}/output/GC3.tab"
+
+gwf.target(sanify(F"GC"),
+	inputs = [], 
+	outputs = [GC3_output_file, 'GC_done'],
+	cores = 1,
+	walltime = '2:00:00',
+	memory = '4gb',
+	account = 'gBGC') << f"""
+
+	cd data/tabseq/genospecies_unitig_genes/fasta
+
+	# Reset output.
+	
+	echo -e header\tGC3\tgs\tunitig\tfile > {GC3_output_file}
+	touch GC_done && rm GC_done
+
+	# Loop through all files and output GC3
+	for i in {{A..E}}; do
+		for j in {{0..3}}; do
+			for k in ${{i}}/${{j}}/*.fasta; do
+				echo $k;
+				cat $k | {base_path}/scripts/fasta_gc.py $i $j $k >> {GC3_output_file}
+			done;
+		done;
+	done
+
+	touch GC_done
+	"""
+
 for sel_gs in ['A', 'B', 'C', 'D', 'E']:
 	for sel_un in ['0', '1', '2', '3']:
 		path = f"data/tabseq/genospecies_unitig_genes/fasta/{sel_gs}/{sel_un}"
 
 		tree_path = tree_base + f"/{sel_gs}/{sel_gs}.fasta.raxml.bestTree"
 
-		for gene in glob.glob(path + '/*.fasta'):
+		for _gene, gene in enumerate(glob.glob(path + '/*.tabseq.fasta', recursive = False)):
 
 			gene_file = gene.split('/')[-1]
 			gene_stem = '.'.join(gene_file.split('.')[:-2]) # I opted to also remove the trivial tabseq part.
@@ -38,37 +69,6 @@ for sel_gs in ['A', 'B', 'C', 'D', 'E']:
 
 			print(gene_file, gene_stem)
 
-			GC3_output_file = f"{base_path}/output/GC3.tab"
-
-			gwf.target(sanify(F"GC_{sel_gs}_{sel_un}_{gene_stem}"),
-				inputs = [gene], 
-				outputs = [GC3_output_file],
-				cores = 1,
-				walltime = '2:00:00',
-				memory = '4gb',
-				account = 'gBGC') << f"""
-
-				echo {gene_stem}
-				ls -l {gene}
-				ls -l {tree_path}
-
-				cd data/tabseq/genospecies_unitig_genes/fasta
-
-				# Reset output.
-				
-				echo -e header\tGC3\tgene\tgs\tunitig\tfile > {GC3_output_file}
-
-				# Loop through all files and output GC3
-				for i in {{A..E}}; do
-					for j in {{0..3}}; do
-						for k in ${{i}}/${{j}}/*.fasta; do
-							echo $k;
-							cat $k | {base_path}/scripts/fasta_gc.py {gene_stem} {sel_gs} {sel_un} $k >> {GC3_output_file}
-						done;
-					done;
-				done
-
-				"""
 
 
 
@@ -89,9 +89,7 @@ for sel_gs in ['A', 'B', 'C', 'D', 'E']:
 				# Make dir.
 				cd {path}
 				mkdir -p {gene_stem}_cf
-				rm -r {gene_stem}_cf
-				mkdir {gene_stem}_cf
-				cd {gene_stem}_cf
+				cd {gene_stem}_cf && rm * || echo empty
 
 
 				# Execute CF.
@@ -100,7 +98,4 @@ for sel_gs in ['A', 'B', 'C', 'D', 'E']:
 
 				# Collect results?
 				"""
-			break
-		break
-	break
 
