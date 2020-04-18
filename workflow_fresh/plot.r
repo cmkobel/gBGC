2291c352ac9d2c9d7fa0864aba4be9c87447b4eb
Carl M. Kobel <kobel@protonmail.ch>
2020-04-18 22:26:06 +0200

    pipeline done and outputs look OK. minor print edits since run

---
+++ workflow_fresh/plot.r
@@ -0,0 +1,4 @@
+library(tidyverse)
+CF <- read_delim("G:/gBGC/carl/workflow_fresh/output/CF_collected.tab", 
+                           "\t", escape_double = FALSE, trim_ws = TRUE)
+
--- workflow_fresh/workflow.py
+++ workflow_fresh/workflow.py
@@ -29,30 +29,30 @@ GC3_output_file = f"{base_path}/output/GC3.tab"
 
 gwf.target(sanify(F"GC"),
 	inputs = [], 
-	outputs = [GC3_output_file, 'GC_done'],
+	outputs = [GC3_output_file],
 	cores = 1,
 	walltime = '2:00:00',
 	memory = '4gb',
 	account = 'gBGC') << f"""
 
-	cd data/tabseq/genospecies_unitig_genes/fasta
+		cd data/tabseq/genospecies_unitig_genes/fasta
 
-	# Reset output.
-	
-	echo -e header\tGC3\tgs\tunitig\tfile > {GC3_output_file}
-	touch GC_done && rm GC_done
+		# Reset output.
+		
+		echo -e "header\tGC3\tgs\tunitig\tfile" > {GC3_output_file}
+		touch GC_done && rm GC_done
 
-	# Loop through all files and output GC3
-	for i in {{A..E}}; do
-		for j in {{0..3}}; do
-			for k in ${{i}}/${{j}}/*.fasta; do
-				echo $k;
-				cat $k | {base_path}/scripts/fasta_gc.py $i $j $k >> {GC3_output_file}
+		# Loop through all files and output GC3
+		for i in {{A..E}}; do
+			for j in {{0..3}}; do
+				for k in ${{i}}/${{j}}/*.fasta; do
+					echo $k;
+					cat $k | {base_path}/scripts/fasta_gc.py $i $j $k >> {GC3_output_file}
+				done;
 			done;
-		done;
-	done
+		done
 
-	touch GC_done
+		touch GC_done
 	"""
 
 for sel_gs in ['A', 'B', 'C', 'D', 'E']:
@@ -67,7 +67,8 @@ for sel_gs in ['A', 'B', 'C', 'D', 'E']:
 			gene_stem = '.'.join(gene_file.split('.')[:-2]) # I opted to also remove the trivial tabseq part.
 
 
-			print(gene_file, gene_stem)
+			#debug
+			#print(gene_file, gene_stem)
 
 
 
@@ -81,21 +82,61 @@ for sel_gs in ['A', 'B', 'C', 'D', 'E']:
 				memory = '4gb',
 				account = 'gBGC') << f"""
 
-				# Debug.
-				echo {gene_stem}
-				ls -l {gene}
-				ls -l {tree_path}
+					# Debug.
+					echo {gene_stem}
+					ls -l {gene}
+					ls -l {tree_path}
 
-				# Make dir.
-				cd {path}
-				mkdir -p {gene_stem}_cf
-				cd {gene_stem}_cf && rm * || echo empty
-
-
-				# Execute CF.
-				ClonalFrameML {base_path}/{tree_path} {base_path}/{gene} {gene_stem}
+					# Make dir.
+					cd {path}
+					mkdir -p {gene_stem}_cf
+					cd {gene_stem}_cf && rm * || echo empty
 
 
-				# Collect results?
+					# Execute CF.
+					ClonalFrameML {base_path}/{tree_path} {base_path}/{gene} {gene_stem}
+
+
+					# Collect results?
+
 				"""
 
+
+
+CF_collect_file = f"{base_path}/output/CF_collected.tab"
+
+gwf.target(sanify(F"ph2_collect_CF"),
+	inputs = [], 
+	outputs = [CF_collect_file],
+	cores = 1,
+	walltime = '2:00:00',
+	memory = '4gb',
+	account = 'gBGC') << f"""
+
+		
+		cd data/tabseq/genospecies_unitig_genes/fasta
+
+		# Reset output.
+		
+		echo "genospecies	unitig	parameter	post_mean	post_var	a_post	b_post" > {GC3_output_file}
+		touch CF_done && rm CF_done
+
+		# Loop through all files and output GC3
+		for i in {{A..E}}; do
+			for j in {{0..3}}; do
+				for k in ${{i}}/${{j}}/*cf/*.em.txt; do
+					echo $k;
+					l=$(basename $k)
+					grep -E "(^nu)|(^1/delta)|(^R/theta)" $k | awk -v vari="$i" -v varj="$j" -v vark="$l" '{{ print vari"\t"varj"\t"vark"\t" $0}}' >> {CF_collect_file}
+					
+				done;
+			
+			done;
+			
+		done
+
+		touch CF_done
+
+
+	"""
+		
